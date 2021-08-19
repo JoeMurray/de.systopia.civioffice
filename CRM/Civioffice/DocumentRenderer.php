7410f9d1dd098d05d320428f17ae766978ac9261
Jo Franz <franz@systopia.de>
2021-08-19 12:17:31 +0200

    Added contribution token code. Unfortunately still crashing

--- CRM/Civioffice/DocumentRenderer.php
+++ CRM/Civioffice/DocumentRenderer.php
@@ -113,10 +113,146 @@ abstract class CRM_Civioffice_DocumentRenderer extends CRM_Civioffice_OfficeComp
              */
 
             return $processor->getRow(0)->render($identifier);
+        } else if ($entity_type == 'contribution') {
+
+            // debug code:
+            $string = "start {contribution.total_amount} end";
+
+            $categories = self::getTokenCategories();
+
+            $messageToken = CRM_Utils_Token::getTokens($string);
+
+            $domain = CRM_Core_BAO_Domain::getDomain();
+            $string = CRM_Utils_Token::replaceDomainTokens($string, $domain, TRUE, $messageToken);
+            $string = CRM_Utils_Token::replaceContactTokens($string, $contact, TRUE, $messageToken);
+
+            // crash:
+
+            $contribution = self::buildContributionArray("", [$entity_id], 1, false, false, $messageToken, self::class, "****~~~~", true);
+
+            $string = CRM_Utils_Token::replaceContributionTokens($string, $contribution, TRUE, $messageToken);
+            //$string = CRM_Utils_Token::replaceHookTokens($string, $contact, $categories, TRUE);
+
+            /*
+            if (defined('CIVICRM_MAIL_SMARTY') && CIVICRM_MAIL_SMARTY) {
+                $smarty = CRM_Core_Smarty::singleton();
+                // also add the tokens to the template
+                $smarty->assign_by_ref('contact', $contact);
+                $string = $smarty->fetch("string:$string");
+            }
+            */
+            return $string;
         }
 
         // todo: implement?
         throw new Exception('replaceAllTokens not implemented for entity ' . $entity_type);
+    }
+
+    /**
+     * Generate the contribution array from the form, we fill in the contact details and determine any aggregation
+     * around contact_id of contribution_recur_id
+     *
+     * @param string $groupBy
+     * @param array $contributionIDs
+     * @param array $returnProperties
+     * @param bool $skipOnHold
+     * @param bool $skipDeceased
+     * @param array $messageToken
+     * @param string $task
+     * @param string $separator
+     * @param bool $isIncludeSoftCredits
+     *
+     * @return array
+     */
+    public static function buildContributionArray($groupBy, $contributionIDs, $returnProperties, $skipOnHold, $skipDeceased, $messageToken, $task, $separator, $isIncludeSoftCredits) {
+            $contributions = $contacts = [];
+
+            $contributionIDs = [95]; //fixme!!
+            $returnProperties = ['contribution.total_amount'];
+
+
+            foreach ($contributionIDs as $item => $contributionId) {
+                $contribution = CRM_Contribute_BAO_Contribution::getContributionTokenValues($contributionId, $messageToken)['values'][$contributionId];
+                $contribution['campaign'] = $contribution['contribution_campaign_title'] ?? NULL;
+                $contributions[$contributionId] = $contribution;
+
+                if (false && $isIncludeSoftCredits) {
+                    //@todo find out why this happens & add comments
+                    [$contactID] = explode('-', $item);
+                    $contactID = (int) $contactID;
+                }
+                else {
+                    $contactID = $contribution['contact_id'];
+                }
+                if (!isset($contacts[$contactID])) {
+                    $contacts[$contactID] = [];
+                    $contacts[$contactID]['contact_aggregate'] = 0;
+                    $contacts[$contactID]['combined'] = $contacts[$contactID]['contribution_ids'] = [];
+                }
+
+                $contacts[$contactID]['contact_aggregate'] += $contribution['total_amount'];
+                $groupByID = empty($contribution[$groupBy]) ? 0 : $contribution[$groupBy];
+
+                $contacts[$contactID]['contribution_ids'][$groupBy][$groupByID][$contributionId] = TRUE;
+                if (!isset($contacts[$contactID]['combined'][$groupBy]) || !isset($contacts[$contactID]['combined'][$groupBy][$groupByID])) {
+                    $contacts[$contactID]['combined'][$groupBy][$groupByID] = $contribution;
+                    $contacts[$contactID]['aggregates'][$groupBy][$groupByID] = $contribution['total_amount'];
+                }
+                else {
+                    $contacts[$contactID]['combined'][$groupBy][$groupByID] = self::combineContributions($contacts[$contactID]['combined'][$groupBy][$groupByID], $contribution, $separator);
+                    $contacts[$contactID]['aggregates'][$groupBy][$groupByID] += $contribution['total_amount'];
+                }
+            }
+            // Assign the available contributions before calling tokens so hooks parsing smarty can access it.
+            // Note that in core code you can only use smarty here if enable if for the whole site, incl
+            // CiviMail, with a big performance impact.
+            // Hooks allow more nuanced smarty usage here.
+            CRM_Core_Smarty::singleton()->assign('contributions', $contributions);
+            foreach ($contacts as $contactID => $contact) {
+                [$tokenResolvedContacts] = CRM_Utils_Token::getTokenDetails(['contact_id' => $contactID],
+                                                                            $returnProperties,
+                                                                            $skipOnHold,
+                                                                            $skipDeceased,
+                                                                            NULL,
+                                                                            $messageToken,
+                                                                            $task
+                );
+                $contacts[$contactID] = array_merge($tokenResolvedContacts[$contactID], $contact);
+            }
+            return [$contributions, $contacts];
+        }
+
+    /**
+     * Get the categories required for rendering tokens.
+     *
+     * @return array
+     */
+    protected static function getTokenCategories() {
+        if (!isset(Civi::$statics[__CLASS__]['token_categories'])) {
+            $tokens = [];
+            CRM_Utils_Hook::tokens($tokens);
+            Civi::$statics[__CLASS__]['token_categories'] = array_keys($tokens);
+        }
+        return Civi::$statics[__CLASS__]['token_categories'];
+    }
+
+    /**
+     * We combine the contributions by adding the contribution to each field with the separator in
+     * between the existing value and the new one. We put the separator there even if empty so it is clear what the
+     * value for previous contributions was
+     *
+     * @param array $existing
+     * @param array $contribution
+     * @param string $separator
+     *
+     * @return array
+     */
+    public static function combineContributions($existing, $contribution, $separator) {
+        foreach ($contribution as $field => $value) {
+            $existing[$field] = isset($existing[$field]) ? $existing[$field] . $separator : '';
+            $existing[$field] .= $value;
+        }
+        return $existing;
     }
 
     /*
